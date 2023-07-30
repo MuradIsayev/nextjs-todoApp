3227690bc7c987d149e23c983f47d36efd077ff2
MurAd <muradisayevazer@gmail.com>
2023-07-31 01:20:48 +0400

    toggle function created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,21 @@ function getTodos() {
   return prisma.toDo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  'use server';
+
+  await prisma.toDo.update({ where: { id }, data: { complete } });
+}
+
 export default async function Home() {
   const todos = await getTodos();
 
   return (
     <>
-      <header className="flex justify-between items-center mb-4 ">
+      <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
         <Link
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700
-        focus-within:bg-slate-700 outline-none"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           href="/new"
         >
           New
@@ -23,7 +28,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
